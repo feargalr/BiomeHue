41e9ddd609d896dc2c01cca3c449933753a921ff
feargalr <feargalr@gmail.com>
2023-03-29 11:30:31 +1030

    Updated help documentation

--- R/biomeHue.R
+++ R/biomeHue.R
@@ -1,22 +1,44 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
-
+#' Get a vector of colours for a microbiota barplot
+#'
+#' This function returns a vector of colours in hexadecimal format for a microbiota barplot. The colours are based on a pre-defined colour palette stored in the 'hue' data frame.
+#'
+#' @param taxa A character vector of bacterial names for which colours are desired.
+#'
+#' @return A character vector of colours in hexadecimal format.
+#'
+#' @examples
+#' # Get the colours for the bacteria 'Bacteroides' and 'Faecalibacterium'
+#' biomeHue(c('Bacteroides', 'Faecalibacterium'))
+#'
+#' # Get the full colour palette
+#' biomeHue_palette()
+#'
+#' @export
 biomeHue <- function(taxa) {
-  return(hue[hue$Taxon %in% taxa,])
+  # Load the 'hue' data frame, which contains the pre-defined colour palette
+  data(hue)
+  # Return the rows of the 'hue' data frame where the 'Taxon' column matches the input 'taxa'
+  return(hue[hue$Taxon %in% taxa, 'Colour'])
 }
 
-biomeHue_palette <- function(taxa) {
+#' Get the full colour palette for microbiota barplots
+#'
+#' This function returns the full pre-defined colour palette for microbiota barplots. The colour palette is stored in the 'hue' data frame.
+#'
+#' @return A data frame with two columns: 'Taxon' (the bacterial name) and 'Colour' (the colour in hexadecimal format).
+#'
+#' @examples
+#' # Get the full colour palette
+#' biomeHue_palette()
+#'
+#' @export
+biomeHue_palette <- function() {
+  # Load the 'hue' data frame, which contains the pre-defined colour palette
+  data(hue)
   return(hue)
 }
+
+
+
+
+
